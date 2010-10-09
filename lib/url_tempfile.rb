92ba71d4a8bd56d4c2305fd552814ed04edf9800
Dmitri Zhuchkov <keeruline@gmail.com>
2010-10-09 10:43:39 +0300

    Basic handling of redirects has been added. It is required to download pictures from facebook where picture URL is always redirected to asset server

--- lib/url_tempfile.rb
+++ lib/url_tempfile.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 # This class provides a Paperclip plugin compliant interface for an "upload" file
 # where that uploaded file is actually coming from a URL.  This class will download
 # the file from the URL and then respond to the necessary methods for the interface,
-# as required by Paperclip so that the file can be processed and managed by 
+# as required by Paperclip so that the file can be processed and managed by
 # Paperclip just as a regular uploaded file would.
 #
 class URLTempfile < Tempfile
@@ -18,18 +18,16 @@ class URLTempfile < Tempfile
   end
 
   BUFFER_SIZE = 1024
-  
+
   attr :content_type
-  
+  attr_reader :original_filename
+
   def initialize(url)
     @url = URI.parse(url)
-    
-    # see if we can get a filename
-    raise "Unable to determine filename for URL uploaded file." unless original_filename
 
     super('urlupload')
-  
-    http(@url).request_get(@url.request_uri) do |res|
+
+    fetch(@url) do |url, res|
       raise UnsuccessfulHTTPResponse.new(url, res) unless res.code.to_s =~ /^2\d\d$/
 
       res.read_body do |segment|
@@ -37,18 +35,29 @@ class URLTempfile < Tempfile
       end
 
       @content_type = res.content_type
+
+      # Take the URI path and strip off everything after last slash, assume this
+      # to be filename (URI path already removes any query string)
+      @original_filename = url.path.split('/').last;
     end
-    
+
     self.flush
   end
-  
-  def original_filename
-    # Take the URI path and strip off everything after last slash, assume this
-    # to be filename (URI path already removes any query string)
-    @url.path[/^.*\/(.+)$/, 1]
-  end
-  
+
   private
+
+  def fetch(uri, limit = 10, &block)
+    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+    http(uri).request_get(uri.request_uri) do |response|
+     case response
+        when Net::HTTPSuccess     then yield uri, response
+        when Net::HTTPRedirection then fetch(URI.parse(response['location']), limit - 1, &block)
+        else raise UnsuccessfulHTTPResponse.new(uri, response)
+      end
+    end
+  end
+
   def http(uri) #:nodoc:
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.is_a?(URI::HTTPS)
--- spec/url_tempfile_spec.rb
+++ spec/url_tempfile_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe URLTempfile do
   let(:path_end)     { 'file.jpg' }
-  let(:url)          { "http://example.com/some/path/#{path_end}" }
+  let(:url_scheme)   { 'http' }
+  let(:url_base)     { "example.com/some/path"}
+  let(:url)          { "#{url_scheme}://#{url_base}/#{path_end}" }
   let(:body)         { 'the-file-body' }
   let(:content_type) { 'image/jpeg' }
   subject            { URLTempfile.new(url) }
 
-  def stub_http_request(status)
+  def stub_http_request(status, url)
     stub_request(:get, url).to_return(
       :body    => body,
       :status  => status,
@@ -15,8 +17,19 @@ describe URLTempfile do
     )
   end
 
+  def stub_http_redirect(status, from, to)
+    from_url = "#{from}://#{url_base}"
+    to_url = "#{to}://#{url_base}/#{path_end}"
+
+    stub_request(:get, from_url).to_return(
+      :status  => status,
+      :headers => { 'location' => to_url }
+    )
+    stub_http_request(200, to_url)
+  end
+
   context "when the request has a 200 status code" do
-    before(:each) { stub_http_request(200) }
+    before(:each) { stub_http_request(200, url) }
 
     its(:content_type)      { should == content_type }
     its(:original_filename) { should == path_end }
@@ -27,9 +40,29 @@ describe URLTempfile do
     end
   end
 
-  [404, 500, 301].each do |status|
+  [
+    {:status => 301, :from => 'http',  :to => 'http'},
+    {:status => 302, :from => 'http',  :to => 'https'},
+    {:status => 303, :from => 'https', :to => 'http'},
+    {:status => 304, :from => 'https', :to => 'https'}
+  ].each do |redirect|
+    context "when the request has a 30x status code" do
+      subject { URLTempfile.new("#{redirect[:from]}://#{url_base}") }
+      before(:each) { stub_http_redirect(redirect[:status], redirect[:from], redirect[:to]) }
+
+      its(:content_type)      { should == content_type }
+      its(:original_filename) { should == path_end }
+
+      it 'has the correct content' do
+        subject.rewind
+        subject.read.should == body
+      end
+    end
+  end
+
+  [404, 500].each do |status|
     context "when the request has a #{status} status code" do
-      before(:each) { stub_http_request(status) }
+      before(:each) { stub_http_request(status, url) }
 
       it 'raises an error' do
         expect { subject }.to raise_error(URLTempfile::UnsuccessfulHTTPResponse)
